cbc2200ea41538f3a24dc5347a885596480936ce
Rodrigo Assis <rodrigo.assis.fialho@gmail.com>
2012-10-22 10:56:28 -0200

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Curtocartao::Application.config.secret_token = '96ad3f773b944c3f88a78d698e158fb2a9cf192af55b0a473865f30b5f39c1465734e9bac3993030682e72def0045fc482778679728b51b7443f6eb47b84ec0b'
