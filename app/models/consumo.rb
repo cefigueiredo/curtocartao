ea393e30e7c8f13ce04ba6d59d237ee635837e83
Rodrigo Assis <rodrigo.assis.fialho@gmail.com>
2013-03-11 10:55:25 -0300

    acrescentando after filter para kissmetrics

--- app/models/consumo.rb
+++ app/models/consumo.rb
@@ -1,8 +1,15 @@
 class Consumo < ActiveRecord::Base
+  after_create :salva_kissmetrics
 	belongs_to :cliente
 
 	scope :da_data, lambda {|data| where("data = ?", data)}
 	scope :resumo, select("produto_id, sum(quantidade) as quantidade").group("produto_id")
 	scope :por_cliente, select("cliente_id, produto_id, sum(quantidade) as quantidade").group("cliente_id, produto_id")
 	scope :no_periodo, lambda {|hora_inicial, hora_final| where("created_at between ? and ?", hora_inicial, hora_final)}
+
+  def salva_kissmetrics
+    produto = Produto.find(self.produto_id)
+    KM.identify(self.cliente.email)
+    KM.record('Purchased', {'Purchased Product Name' => produto.nome, 'Quantity' => self.quantidade, 'Price' => produto.preco * self.quantidade })
+  end
 end
---
+++ config/initializers/km.rb
@@ -0,0 +1,1 @@
+KM.init("7b026a4bcd4c18460b54a6022b04ff5a44617d1a")
