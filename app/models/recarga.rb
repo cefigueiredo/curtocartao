3a1b5ba5e525534f198e5b8204700b2d66043f0b
Pedro Howat <pedro.howat@gmail.com>
2013-05-14 23:02:58 -0300

    Mostrando as recargas registradas na nova tabela de recargas no contador de consumo.

--- app/models/recarga.rb
+++ app/models/recarga.rb
@@ -1,3 +1,5 @@
 class Recarga < ActiveRecord::Base
   belongs_to :cliente
+	scope :da_data, lambda {|data| where("DATE(created_at) = ?", data)}
+	scope :no_periodo, lambda {|hora_inicial, hora_final| where("created_at between ? and ?", hora_inicial, hora_final)}
 end
