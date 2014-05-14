ffccf3992dbfcdb0e840ec9d6c09e49275295ddd
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-05-14 18:18:37 -0300

    Extracted js to specific file.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,17 +7,3 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-
-$(document).ready(function($){
-
-  $("#recharge_submit").click(function(){
-    var current_modal = $(this).parents(".modal");
-    var url = current_modal.data("url");
-
-    $.post(url, function(data) {
-      $("#saldo-f").html(data.saldo);
-      current_modal.modal("hide");
-    });
-  });
-
-});
