7c8c02c4b06cb01c589e07c4c82d37aad6c15c89
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-05-14 14:12:55 -0300

    Configure factory-girl syntax.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,4 +28,6 @@ RSpec.configure do |config|
   config.order = "random"
   config.render_views
   config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.include FactoryGirl::Syntax::Methods
+
 end
