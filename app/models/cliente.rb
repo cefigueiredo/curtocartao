b89c4a6c87ee4e075f8f6491f4b4b2f49436b212
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-05-14 22:30:31 -0300

    Add validations to cliente.

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -2,4 +2,7 @@ class Cliente < ActiveRecord::Base
 	has_many :consumos
   has_many :contribuicaos
   has_many :recargas
+
+  # TODO: Testar unitariamente após merge do branch com rspec
+  validates :nome, :email, presence: true
 end
