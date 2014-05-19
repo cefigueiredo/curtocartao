597af46e0a9f7e2845649999c4108b266653e4e4
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-05-19 15:45:57 -0300

    Busca cliente no before_filter para remover repetição.

--- app/controllers/recargas_controller.rb
+++ app/controllers/recargas_controller.rb
@@ -1,35 +1,33 @@
 class RecargasController < ApplicationController
 
-  def cinco_reais
-    @cliente = Cliente.find(params[:cliente_id])
-    @cliente.recarregar_cinco_reais
+  before_filter :load_cliente
 
+  def cinco_reais
+    @cliente.recarregar_cinco_reais
     render json: { saldo: number_to_currency(@cliente.saldo) }.to_json
   end
 
   def dez_reais
-    @cliente = Cliente.find(params[:cliente_id])
     @cliente.recarregar_dez_reais
-
     render json: { saldo: number_to_currency(@cliente.saldo) }.to_json
   end
 
   def vinte_reais
-    @cliente = Cliente.find(params[:cliente_id])
     @cliente.recarregar_vinte_reais
-
     render json: { saldo: number_to_currency(@cliente.saldo) }.to_json
   end
 
   def cinquenta_reais
-    @cliente = Cliente.find(params[:cliente_id])
     @cliente.recarregar_cinquenta_reais
-
     render json: { saldo: number_to_currency(@cliente.saldo) }.to_json
   end
 
   private
     def number_to_currency(value)
       ActionController::Base.helpers.number_to_currency(value)
+    end
+
+    def load_cliente
+      @cliente = Cliente.find(params[:cliente_id])
     end
 end
