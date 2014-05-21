cce713e2f5ad71af882051b9706912de58021098
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-05-20 22:28:41 -0300

    Add enforce available locales to true for next rails versions.

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module Curtocartao
       g.test_framework :rspec
     end
 
+    config.i18n.enforce_available_locales = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
