970f1804c744eaa816ccbcff499087363781dbd6
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-05-27 01:06:27 -0300

    Add validations to produto.

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,2 +1,9 @@
 class Produto < ActiveRecord::Base
+
+  # TODO: Testar unitariamente
+  validates :nome, presence: true
+
+  validates :preco, numericality: { greater_than_or_equal_to: 0.0 }
+  validates :ordem, numericality: { greater_than: 0 }
+
 end
